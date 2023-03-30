88ddc2d43f9650a567b11c40bea845b5d63552bc
6Yoru6 <carluantoine66@outlook.com>
2023-03-30 12:46:10 +0200

    Better working and indices given added

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-//Randomly pick a number between 1 and 10
-const secretNumber = Math.floor(Math.random() * 10) + 1;
-
+let secretNumber = Math.floor(Math.random() * 10) + 1;
 let remainingTries = 3;
 
 function check() {
-  const guess = document.getElementById("guess").value;
+	const guessInput = document.getElementById("guess");
+	const resultDiv = document.getElementById("result");
+	const guess = parseInt(guessInput.value);
 
-  //Error message if the user write an unvalid number between 1 and 10
-  if (isNaN(guess) || guess < 1 || guess > 10 || guess.indexOf(".") !== -1) {
-    alert("Vous avez selectionné un nombre invalide, prenez un nombre entier entre 1 et 10");
-    return;
-  }
+	if (isNaN(guess) || guess < 1 || guess > 10) {
+		alert("Veuillez saisir un nombre valide entre 1 et 10.");
+		return;
+	}
 
-  //Reduce tries remaining when enter a wrong number to guess
-  remainingTries = remainingTries - 1
+	if (guess === secretNumber) {
+		resultDiv.innerHTML = `Bravo ! Vous avez gagné en ${4 - remainingTries} essai(s).`;
+		document.getElementById("reset").style.display = "block";
+		document.getElementById("guess").disabled = true;
+		document.getElementById("guess").value = "";
+	} else {
+		remainingTries = remainingTries - 1;
+		if (remainingTries > 0 && guess > secretNumber) {
+			resultDiv.innerHTML = `Raté. Le nombre secret est plus petit. Il vous reste ${remainingTries} essai(s).`;
+		} else if (remainingTries > 0 && guess < secretNumber) {
+            resultDiv.innerHTML = `Raté. Le nombre secret est plus grand. Il vous reste ${remainingTries} essai(s).`;
+            } else {
+                resultDiv.innerHTML = `Perdu ! Il ne vous reste plus d'essais. Le nombre secret était ${secretNumber}.`;
+                document.getElementById("reset").style.display = "block";
+                document.getElementById("guess").disabled = true;
+                document.getElementById("guess").value = "";
+            }
+	}
+}
 
-  //Check if the number is the same as secretNumber and display result
-  if (guess == secretNumber) {
-    document.getElementById("result").innerHTML = "Bravo ! Vous avez trouvé le bon nombre avec " + (3 - remainingTries) + " essai(s) !";
-    return;
-  } else if (remainingTries == 0) {
-    document.getElementById("result").innerHTML = "Perdu ! Il ne vous reste plus d'essais. Le nombre secret était " + secretNumber + ".";
-    return;
-  } else {
-    document.getElementById("result").innerHTML = "Dommage, ce n'est pas le bon nombre. Il vous reste " + remainingTries + " essai(s) restant(s).";
-    return;
-  }
+function reset() {
+	secretNumber = Math.floor(Math.random() * 10) + 1;
+	remainingTries = 3;
+	document.getElementById("result").innerHTML = "";
+	document.getElementById("reset").style.display = "none";
+	document.getElementById("guess").disabled = false;
 }
